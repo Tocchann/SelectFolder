88024041cd326193788f15956df9daedf3151196
Tocchann <tosiyuki@hh.iij4u.or.jp>
2020-07-27 15:41:53 +0900

    プロジェクト ファイルを追加します。

---
+++ SelectFolder/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SelectFolder
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click( object sender, RoutedEventArgs e )
+		{
+			var dlg = new PickupFolderDialog.PickupFolderDialog();
+			dlg.SelectedPath = EditSelFolder.Text;
+			if( dlg.ShowDialog( this ) )
+			{
+				EditSelFolder.Text = dlg.SelectedPath;
+			}
+		}
+
+		private void Button_Click_Cancel( object sender, RoutedEventArgs e )
+		{
+			Close();	//	終了する
+		}
+	}
+}
